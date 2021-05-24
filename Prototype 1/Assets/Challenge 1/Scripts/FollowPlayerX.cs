cc2fd8d1c80adfbb6a0ab41fdd7c12f620481385
ilya <oblogin.ilya@gmail.com>
2021-05-24 23:33:45 +0300

    Challenge 1 - Plane Programming

---
+++ Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 45.0f;
+    public float verticalInput;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // получить вертикальный ввод пользователя
+        verticalInput = Input.GetAxis("Vertical");
+
+        // перемещать самолет вперед с постоянной скоростью
+        transform.Translate(Vector3.forward * (Time.fixedDeltaTime * speed * Time.timeScale));
+
+        // наклон самолета вверх/вниз на основе клавиш со стрелками вверх/вниз
+        transform.Rotate(Vector3.left, verticalInput * rotationSpeed * Time.fixedDeltaTime * Time.timeScale);
+    }
+}
